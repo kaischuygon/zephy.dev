13933bbd82681b30640480b58a31d30396fb90da
Kai Schuyler Gonzalez <kai.schuyler@gmail.com>
2025-05-08 02:18:05 -0600

    scaffolding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import react from '@astrojs/react';
+import db from '@astrojs/db';
+import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), db(), markdoc()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
