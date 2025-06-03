a56ec07e94d85de233f50bf615fa0dae7b9b6844
Kai Schuyler Gonzalez <kai.schuyler@gmail.com>
2025-06-03 02:30:50 -0600

    links, about and index page"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 import db from '@astrojs/db';
 import markdoc from '@astrojs/markdoc';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), db(), markdoc()],
+  integrations: [react(), db(), markdoc(), icon({include: {bx: ['*']}})],
   vite: {
     plugins: [tailwindcss()]
   }
